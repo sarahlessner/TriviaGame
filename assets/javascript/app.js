c1e4354966c7a4b2c3d052dd7042a34101035c72
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-05-23 00:33:05 -0700

    basic functionality complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,220 @@
-//create an array of questions
-var questionsArray = ["1", "2", "3", "4"];
-//create an array of answer sets at the same index as the corresponding question
-var answersArray = [
-	["1-a1", "1-a2", "1-a3", "1-a4"],
-	["2-a1", "2-a2", "2-a3", "2-a4"],
-	["3-a1", "3-a2", "3-a3", "3-a4"],
-	["4-a1", "4-a2", "4-a3", "4-a4"],
-];
-var correctsAnswers = [2,0,3,1];
+//create a multi dimensional array
+//questions at index 0
+//choices at 1-4
+//value for correct answer at 5
+var qnaArray = [
 
-//create an array of photos that will display with correct answer
+	["Who sang x?", "Band Ax", "Band Bx", "Band Cx", "Band Dx", "1"],
+	["Who sang y?", "Band Ay", "Band By", "Band Cy", "Band Dy", "2"],
+	["Who sang z?", "Band Az", "Band Bz", "Band Cz", "Band Dz", "3"],
+	["Who sang q?", "Band Aq", "Band Bq", "Band Cq", "Band Dq", "4"]
+
+];
+
+//TODO: create an array of photos that will display with correct answer
 
 //create global variables for:
 //index of the question/answer pair
-var questionIdx = 0;
-//timer
-var timer;
+var questionIdx;
+var correctAnswerIdx;
 //countdown for seconds to countdown time to answer question
-var countdown = 30;
+var countdown;
+var timer;
 //variable to store correct/incorrect selection count
 var correct = 0;
 var incorrect = 0;
-$("#correct").text(correct);
-$("#incorrect").text(incorrect);
 
 
-//create onclick function for start button
+$("button").on("click", beginGame);
 
-	//hide start button
-
+//create function for new game click event
+function beginGame() {
+	//set questionIdx to zero
+	questionIdx = 0;
+	//clear display of correct/incorrect and set to 0
+	correct = 0;
+	$("#correct").empty();
+	incorrect = 0;
+	$("#incorrect").empty();
+	$("#final-result").empty();
+	//hide new game button
+	$("#start").empty();
 	//calls function for a new question/answer pair
+	displayQ();
+}
 
 //create function for displaying question/answer pair
-//will display on click - start and 5 second delay with timer re-set
 
-	//make question-answer container visible
+function displayQ() {
 
-	//calls function for timer countdown when new question/answer is displayed
+	$("#qresult").empty();
 
-	//jquery to display "questions" at questionIdx
+	if (questionIdx === qnaArray.length) {
+		$("#countdown").empty();
+		$("#correct").append("You got "+correct+" questions right");
+		$("#incorrect").append("You got "+incorrect+" questions wrong");
+		$("#final-result").text("GAME OVER!");
+		$("#start").html("<button onclick='beginGame()'>Try Again</button>");
+	}
 
-	//variable to store "answers" at questionIdx
-	var answers = answersArray[questionIdx];
+	else {
+		//store the selection of the q-and-a element as a variable since I need it a bunch
+		var qandA = $("#q-and-a");
+		//create variables to store result of grabbing question/indexes from multi-dimensional array
+		var question = qnaArray[questionIdx][0];
+		var ans1 = qnaArray[questionIdx][1];
+		var ans2 = qnaArray[questionIdx][2];
+		var ans3 = qnaArray[questionIdx][3];
+		var ans4 = qnaArray[questionIdx][4];
+		//display question and answers to user
+		qandA.html("<h2>"+question+"</h2>");
+		qandA.append("<input type='radio' name='answers' value='1'>"+ans1+"<br>");
+		qandA.append("<input type='radio' name='answers' value='2'>"+ans2+"<br>");
+		qandA.append("<input type='radio' name='answers' value='3'>"+ans3+"<br>");
+		qandA.append("<input type='radio' name='answers' value='4'>"+ans4+"<br>");
+		qandA.append("<button onclick='submitAnswer()'>Submit</button>");
 
-	//use jquery to put answers in corresponding input element
+
+		//set countdown variable
+		countdown = 20;
+		//set interval for countdown
+		timer = setInterval(hourGlass, 1000);
+		//display countdown in html
+		console.log(countdown);
+		$("#countdown").text(countdown);
+	}
+}
+
+
+//function for the question timer countdown
+	
+function hourGlass() {
+
+	//decrement "countdown"
+	countdown--;
+	//display "countdown"
+	$("#countdown").text(countdown);
+	//if countdown is 0 clear the interval, increase incorrect count and display result
+	if (countdown === 0) {
+		$("#qresult").html("TIME'S UP!");
+		clearInterval(timer);
+		$("#q-and-a").empty();
+		incorrect++;
+		displayAnswer();
+	}
+
+
+}
+
+
+
+//create an on click event for answer submission
+function submitAnswer() {
+	clearInterval(timer);
+	correctAnswerIdx = qnaArray[questionIdx][5];
+	var answers = document.getElementsByName("answers");
+	var answer = -1;
+	for (var i=0; i<answers.length; i++) {
+		if (answers[i].checked) {
+			answer = answers[i].value;
+		}
+	}
+	if (answer == -1) {
+		alert("Choose an answer!");
+	}
+	else {
+		if (answer == correctAnswerIdx) {
+		correct++;
+		$("#q-and-a").empty();
+		$("#qresult").text("CORRECT!");
+		//display photo at questionIdx
+		//display answer
+		displayAnswer();
+		}
+		else {
+		incorrect++;
+		$("#q-and-a").empty();
+		$("#qresult").text("WRONG!");
+		//display photo at questionIdx
+		//display answer
+		displayAnswer();
+		}
+
+		
+	}
+// call function that sets a timeout to display new question
+
+};
+
+//function to get correct answer
+
+function displayAnswer() {
+
+	correctAnswerIdx = qnaArray[questionIdx][5];
+	correctAnswerIdx = parseInt(correctAnswerIdx);
+	var correctAnswerText = qnaArray[questionIdx][correctAnswerIdx];
+	$("#qresult").append(" "+correctAnswerText);
+	questionIdx++;
+	setTimeout(displayQ, 3500);
+
+};
+
+
+
+
+//function to display final game stats
+
+	//jquery to display incorrect and correct answers count to HTML
+
+	//display new game button for user to play again
 
 
 
 
 
 
+//function to display and adjust various counters
 
-//create function to run an interval timer that counts down time to make a selection
+	//clearInterval
+
+	//highlight or otherwise display correct answer and a corresponding photo
+
+	//if countdown is 0
+
+		//display message that time is up
+
+		//increase incorrect answer count
+
+	// else
+
+		//if value of user answer is equal to correct answer array at questionIdx
+
+			//display message that user is correct
+
+			//increase correct answer count
+
+			
+		//else
+
+			//display message that they are incorrect
+
+			//increase incorrect answer count
+
+	//increment questionIdx
+
+	//set timer to display results page or new question after 5 seconds
+
+		//check if all questions have displayed (questionIdx <= # of questions)
+
+			//if they have, then call function that displays final game stats
+
+			//if not, call function that displays a new question/answer set
+
+
+
+
 
 	
-	//stop timer if 
 
 
 
