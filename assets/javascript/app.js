cf72a0a572300570b325fbbf6473289617959524
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-05-24 19:19:11 -0700

    about to style this game pushing in case I break everything

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,21 +58,22 @@ function displayQ() {
 	}
 
 	else {
-		//store the selection of the q-and-a element as a variable since I need it a bunch
-		var qandA = $("#q-and-a");
+
 		//create variables to store result of grabbing question/indexes from multi-dimensional array
 		var question = qnaArray[questionIdx][0];
-		var ans1 = qnaArray[questionIdx][1];
-		var ans2 = qnaArray[questionIdx][2];
-		var ans3 = qnaArray[questionIdx][3];
-		var ans4 = qnaArray[questionIdx][4];
 		//display question and answers to user
-		qandA.html("<h2>"+question+"</h2>");
-		qandA.append("<input type='radio' name='answers' value='1'>"+ans1+"<br>");
-		qandA.append("<input type='radio' name='answers' value='2'>"+ans2+"<br>");
-		qandA.append("<input type='radio' name='answers' value='3'>"+ans3+"<br>");
-		qandA.append("<input type='radio' name='answers' value='4'>"+ans4+"<br>");
-		qandA.append("<button onclick='submitAnswer()'>Submit</button>");
+		$("#question").html("<h2>"+question+"</h2>");
+		
+		for (i=1; i<(qnaArray[questionIdx].length-1); i++) {
+
+			var ans = qnaArray[questionIdx][i];
+
+			$("#choices").append("<label class='choices'><input type='radio' class='radiobtn' name='answers' value='"+i+"'/>"+ans+"</label><br>");
+		
+		};
+		//submit button
+		$("#submit").append("<button class='submit' onclick='submitAnswer()'>Submit</button>");
+
 
 
 		//set countdown variable
@@ -82,6 +83,7 @@ function displayQ() {
 		//display countdown in html
 		console.log(countdown);
 		$("#countdown").text(countdown);
+		debugger;
 	}
 }
 
@@ -98,7 +100,9 @@ function hourGlass() {
 	if (countdown === 0) {
 		$("#qresult").html("TIME'S UP!");
 		clearInterval(timer);
-		$("#q-and-a").empty();
+		$("#question").empty();
+		$("#choices").empty();
+		$("#submit").empty();
 		incorrect++;
 		displayAnswer();
 	}
@@ -110,7 +114,6 @@ function hourGlass() {
 
 //create an on click event for answer submission
 function submitAnswer() {
-	clearInterval(timer);
 	correctAnswerIdx = qnaArray[questionIdx][5];
 	var answers = document.getElementsByName("answers");
 	var answer = -1;
@@ -123,9 +126,12 @@ function submitAnswer() {
 		alert("Choose an answer!");
 	}
 	else {
+		clearInterval(timer);
 		if (answer == correctAnswerIdx) {
 		correct++;
-		$("#q-and-a").empty();
+		$("#question").empty();
+		$("#choices").empty();
+		$("#submit").empty();
 		$("#qresult").text("CORRECT!");
 		//display photo at questionIdx
 		//display answer
@@ -133,7 +139,9 @@ function submitAnswer() {
 		}
 		else {
 		incorrect++;
-		$("#q-and-a").empty();
+		$("#question").empty();
+		$("#choices").empty();
+		$("#submit").empty();
 		$("#qresult").text("WRONG!");
 		//display photo at questionIdx
 		//display answer
@@ -245,34 +253,3 @@ function displayAnswer() {
 
 
 
-
-
-// var questionList = [
-// 	"Q1",// Question 1
-// 	"Q2",
-// 	"Q3"
-// 	];
-// var answerList = [
-// 	["Q1-A1","Q1-A2","Q1-A3","Q1-A4"],//  Answers for Question 1
-// 	["Q2-A1","Q2-A2","Q2-A3","Q2-A4", "Q2-A5"],
-// 	["Q3-A1","Q3-A2","Q3-A3"],
-// ];
-
-// // Select your Q/A index, and fetch Q and As to internal variables
-// var randIdx = ...normal random stuff...;
-// var randQ = questionList[randIdx];	// The actual question String
-// var randAList = answerList[randIdx];
-
-
-// // Display stuff to user
-// $("#textQ").text(randQ); // Question string pushed to page
-
-// $("#textA0").text(randAList[0]);	// Answer 1 string pushed ot page
-// $("#textA1").text(randAList[1]);	// Answer 1 string pushed ot page
-// $("#textA2").text(randAList[2]);	// Answer 1 string pushed ot page
-// $("#textA3").text(randAList[3]);	// Answer 1 string pushed ot page
-
-// for(var i = 0; i < randAList.length; i++) {
-// 	var name = "#textA" + i.toString();
-// 	$(name).text(randAList[i]);
-// }
